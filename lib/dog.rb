c0f8538b28f5f7baf35c945f5868134cdd4192f0
Baktiar Miah <baktiar.m@gmail.com>
2019-11-09 23:59:01 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -46,7 +46,7 @@ def self.create(name, breed)
  end
 
 
- def self.find_by_name(name)
+ def self.find_by_name(:name)
   sql = "SELECT * FROM dogs WHERE name = ?"
   DB[:conn].execute(sql, name).map { |row| new_from_db(row) }.first
 end
