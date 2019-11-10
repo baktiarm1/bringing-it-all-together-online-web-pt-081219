c639730a8257e9a8eac43fe6747b1ffc4591c908
Baktiar Miah <baktiar.m@gmail.com>
2019-11-10 00:05:36 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -24,9 +24,9 @@ def self.drop_table
  end
 
  def save
-    if self.id
-      self.update
-    else
+    # if self.id
+    #   self.update
+    # else
       sql = <<-SQL
         INSERT INTO dogs (name, breed)
         VALUES (?,?)
@@ -34,7 +34,7 @@ def self.drop_table
 
       DB[:conn].execute(sql, self.name, self.breed)
       @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
-    end
+    #end
   end
 
 
