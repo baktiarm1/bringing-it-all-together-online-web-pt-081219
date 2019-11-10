06b194399b6d19b83730b034a7e5368819bf63c9
Baktiar Miah <baktiar.m@gmail.com>
2019-11-10 00:08:28 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -45,6 +45,13 @@ def self.create(hash)
    dog
  end
 
+ def self.new_from_db(row)
+   id = row[0]
+   name = row[1]
+   breed = row[2]
+   self.new(id, name, breed)
+ end
+
 
  def self.find_by_name(name:)
   sql = "SELECT * FROM dogs WHERE name = ?"
